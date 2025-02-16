114521ce8bdc873d8148ea2b4552824ba446b360
Adrian Boccardo <adrianboccardo@gmail.com>
2025-02-16 15:48:02 +0100

    Update ferritem6.m

--- ferritem6.m
+++ ferritem6.m
@@ -1,7 +1,20 @@
 function [xi,vfFpaut,vfFpgb,vfFpi,vfAfi,vfAbi,cGammablocki]=ferritem6(vfFpi,xfpab,cGamma,cAlphap,cGammaTo,GMAX,T,xi,Bk,vol,up,deltat,vfFpaut,Ak,vfFpgb,vfFpgbmax,vfFpmax,rGr,rA,rAusf,denA,denF,denATo,denAb)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%   ferrite growth
+% Ferrite growth
+% Copyright (C) 2015 Adrian Boccardo (adrianboccardo@gmail.com)
 %
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
