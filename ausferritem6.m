6294e67c3c44881b9584db37858ea302628977d5
Adrian Boccardo <adrianboccardo@gmail.com>
2025-02-16 15:47:36 +0100

    Update ausferritem6.m

--- ausferritem6.m
+++ ausferritem6.m
@@ -1,6 +1,20 @@
 function [rAusf]=ausferritem6(GMAX,T,cGammablocki,cGammaTo,cGamma,plength,deltat,rAusf,rA,vfAusflast,rGr,vfGr,imeff,vfA,vfAini)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%   ausferrite growth
+% Ausferrite growth
+% Copyright (C) 2015 Adrian Boccardo (adrianboccardo@gmail.com)
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
 %
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
