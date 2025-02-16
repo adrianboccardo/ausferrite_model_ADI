d4b5313e7a67ce1577038271722160d38d4a007b
Adrian Boccardo <adrianboccardo@gmail.com>
2025-02-16 15:49:26 +0100

    Update impedimentF_new.m

--- impedimentF_new.m
+++ impedimentF_new.m
@@ -1,14 +1,26 @@
 function [rAusf]=impedimentF_new(vfi,rAusflast,rA,drAusf)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-%   this function calculates the ausferrite volume fraction and its
-%   radius, taking into account the impediment effect
+% Impediment
+% Copyright (C) 2015 Adrian Boccardo (adrianboccardo@gmail.com)
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-%
-%
-%
+% This function calculates the ausferrite volume fraction and its
+% radius, taking into account the impediment effect
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % author: a.d. boccardo
