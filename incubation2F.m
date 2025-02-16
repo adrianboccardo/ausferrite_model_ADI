4562678542baca4b8b26adf3031d9c5d3994808d
Adrian Boccardo <adrianboccardo@gmail.com>
2025-02-16 15:51:37 +0100

    Update incubation2F.m

--- incubation2F.m
+++ incubation2F.m
@@ -2,13 +2,27 @@ function [tnn]=incubation2F(temp,k)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-%   this function calculates the incubation time of a ferrite platelet
-%   acconding to model 2 (proposed time)
+% Incubation 2
+% Copyright (C) 2015 Adrian Boccardo (adrianboccardo@gmail.com)
+%
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <https://www.gnu.org/licenses/>.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% This function calculates the incubation time of a ferrite platelet
+% according to model 2 (proposed time)
 % ttn= type of nucleation time ttn=1 (fixed), ttn=2 (linear increment)
-%
-%
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % author: a.d. boccardo
